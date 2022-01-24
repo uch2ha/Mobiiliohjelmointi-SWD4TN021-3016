e9cd2d1d0891e28ef0d57d0e92daf080c7092388
uch2ha <sinyavskiy.d@gmail.com>
2022-01-24 11:06:10 +0200

    done

--- App.js
+++ App.js
@@ -1,24 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import NumericInput from "react-native-numeric-input";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const [number1, handleInput1] = useState(null);
+  const [number2, handleInput2] = useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>First calculator</Text>
+
+      <NumericInput onChange={handleInput1} autoFocus />
+      <NumericInput onChange={handleInput2} />
+
+      <View style={styles.btnBox}>
+        <View style={styles.btn}>
+          <Button
+            title="+"
+            onPress={() => Alert.alert("Answer is " + (number1 + number2))}
+          />
+        </View>
+        <View style={styles.btn}>
+          <Button
+            title="-"
+            onPress={() => Alert.alert("Answer is " + (number1 - number2))}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: "red",
-    fontSize: "3rem"
-  }
+    paddingBottom: 10,
+    fontSize: 25,
+  },
+  btnBox: {
+    display: "flex",
+    flexDirection: "row",
+    padding: 10,
+  },
+  btn: {
+    padding: 10,
+    width: "15%",
+  },
 });
