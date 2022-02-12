b67d28ffbd7040cc1ad0c0cf7e5d3e370a7a7f84
uch2ha <sinyavskiy.d@gmail.com>
2022-02-12 12:58:29 +0200

    done

--- App.js
+++ App.js
@@ -1,24 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  TextInput,
+  Button,
+  Image,
+} from "react-native";
+import uuid from "react-native-uuid";
+
+const API = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
 
 export default function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [ingridient, setIngridient] = useState("chicken");
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = () => {
+    fetch(API + ingridient)
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.meals));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Recipe App</Text>
+      <View>
+        <TextInput
+          style={styles.input}
+          value={ingridient}
+          onChangeText={setIngridient}
+        />
+        <Button title="FIND" onPress={getRecipes} />
+      </View>
+      {recipes ? (
+        <>
+          {recipes.map((recipe) => (
+            <View key={uuid.v4()}>
+              <Text style={styles.recipeTitle}>{recipe.strMeal}</Text>
+              <Image source={{ uri: recipe.strMealThumb }} style={styles.img} />
+            </View>
+          ))}
+        </>
+      ) : (
+        <></>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 15,
   },
   text: {
-    color: "red",
-    fontSize: "3rem"
-  }
+    fontSize: 30,
+    paddingBottom: 10,
+  },
+  recipeTitle: {
+    fontSize: 20,
+    alignSelf: "center",
+    paddingTop: 10,
+    paddingBottom: 5,
+  },
+  img: {
+    width: 300,
+    height: 300,
+  },
+  input: {
+    textAlign: "center",
+    padding: 5,
+    borderWidth: 1,
+    marginBottom: 10,
+  },
 });
